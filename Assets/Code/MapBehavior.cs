b8bd5d577e10d214601ff18d90f5f2f4f61d5edb
Mads Østermark <mads9220@gmail.com>
2019-11-21 12:25:59 +0100

    ehm

--- Assets/Code/MapBehavior.cs
+++ Assets/Code/MapBehavior.cs
@@ -28,47 +28,24 @@ namespace Deenee {
             base.OnUpdate(onUpdateProperties);
 
             if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.L)) {
-                StandaloneFileBrowser.OpenFilePanelAsync("Load File", "", new[] {
-                    new ExtensionFilter("3D files", "obj"),
-                    new ExtensionFilter("Image files", "png", "jpg", "jpeg")
-                }, true, (string[] paths) => {
+                ExtensionFilter objFilter = new ExtensionFilter("3D files", "3D", "3DS", "3MF", "AC", "AC3D", "ACC", "AMF", "AMJ", "ASE", "ASK", "B3D", "BLEND", "BVH", "COB", "DAE", "DXF", "ENFF", "FBX", "IRR", "LWO", "LWS", "LXO", "MD2", "MD3", "MD5", "MDC", "MDL", "MESH", "MOT", "MS3D", "NDO", "NFF", "OBJ", "OFF", "OGEX", "PLY", "PMX", "PRJ", "Q3O", "Q3S", "RAW", "SCN", "SIB", "SMD", "STL", "TER", "UC", "VTA", "X", "X3D", "XGL", "ZGL");
+                ExtensionFilter imageFilter = new ExtensionFilter("Image files", "png", "jpg", "jpeg");
+                StandaloneFileBrowser.OpenFilePanelAsync("Load File", "", new ExtensionFilter[] {objFilter, imageFilter }, true, (string[] paths) => {
                     paths.ToList().ForEach(path => {
-                        Console.WriteLine(path);
                         string name = new FileInfo(path).Name;
                         string[] split = name.Split('.');
                         string nameWOext = split[0];
                         string ext = split[1];
-                        if (ext == "obj") {
+                        if (objFilter.Extensions.Contains(ext)) {
                             byte[] data = File.ReadAllBytes(path);
                             Main.campaign.AddObject(name, data);
                             InvokeFunction(() => {
                                 using (var assetLoader = new AssetLoader()) {
-                                    var assetLoaderOptions = AssetLoaderOptions.CreateInstance();   //Creates the AssetLoaderOptions instance.
-                                                                                                    //AssetLoaderOptions let you specify options to load your model.
-                                                                                                    //(Optional) You can skip this object creation and it's parameter or pass null.
-
-                                    //You can modify assetLoaderOptions before passing it to LoadFromFile method. You can check the AssetLoaderOptions API reference at:
-                                    //https://ricardoreis.net/trilib/manual/html/class_tri_lib_1_1_asset_loader_options.html
-                                    //(Optional) You can skip this object creation and it's parameter or pass null.
-                                    UpdateCache();
+                                    var assetLoaderOptions = AssetLoaderOptions.CreateInstance();
                                     var myGameObject = assetLoader.LoadFromMemory(data, name, assetLoaderOptions, cachedObjectHandler);
+                                    UpdateCache();
                                 }
                             });
-
-                            /*
-                            var obj = RawObject.Create(File.ReadAllText(path));
-                            string name = nameWOext;
-                            int index = 1;
-                            while (Map.Campaign.RawObjects.FirstOrDefault(x => x.Name == name) != null) {
-                                name = nameWOext + "~" + index++;
-                            }
-                            int id = Map.Campaign.RawObjects.Count;
-                            Map.Campaign.RawObjects.Add(obj);
-                            Token a = new Token();
-                            a.RawObjectId = id;
-                            a.Map = Map;
-                            a.Create();
-                            */
                         }
                         else {
 
